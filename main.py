a13dc78a56eede4b28a4e4200142aa26137a3aa7
yifeizhao18 <122106660+yifeizhao18@users.noreply.github.com>
2023-03-10 15:28:56 +0000

    I added an array for my map

---
+++ main.py
@@ -0,0 +1,10 @@
+row = 1
+col = 3
+
+HanutedMansion = [
+  ["Kitchen", "Rooftop", "Bathroom", "Master Bedroom"]
+  ["Closet", "Grand Ball Room", "Kitchen", "Backyeard"]
+  ["Bedroom", "Bathroom", "Entrance", "Living Room"]
+  ["Guest Room", "Closet", "Entrance Hall", "Laundry Room"]
+  ["Rooftop", "Master Bedroom", "Grand Ball Room", "Exit"]
+]
