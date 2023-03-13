219cc054b6ad7fe1dc197232d6f184499d285c38
yifeizhao18 <122106660+yifeizhao18@users.noreply.github.com>
2023-03-13 15:21:53 +0000

    I added a function for movements; added 3 if statements for the locations on the map; changed the location on the map a little; added a list for directions; added messages that will be printed when the user is in the specific room.

--- main.py
+++ main.py
@@ -1,10 +1,57 @@
-row = 1
-col = 3
+# Global Variables & Imports
+row = 2
+col = 2
 
-HanutedMansion = [
-  ["Kitchen", "Rooftop", "Bathroom", "Master Bedroom"]
-  ["Closet", "Grand Ball Room", "Kitchen", "Backyeard"]
-  ["Bedroom", "Bathroom", "Entrance", "Living Room"]
-  ["Guest Room", "Closet", "Entrance Hall", "Laundry Room"]
+# Functions
+HauntedMansion = [
+  ["Grand Ball Room", "Master Bedroom", "Bathroom", "Master Bedroom"]
+  ["Closet", "Master Bedroom", "Rooftop", "Closet"]
+  ["Grand Ball Room", "Bathroom", "Entrance", "Bathroom"]
+  ["Master Bedroom", "Closet", "Rooftop", "Bathroom"]
   ["Rooftop", "Master Bedroom", "Grand Ball Room", "Exit"]
 ]
+
+directions = ["South", "North", "East", "West"]
+
+welcomeMessage = ("Welcome To The Haunted House! You are currently at the Entrance. ")
+askDirection = ("Do you want to go South, North, East, or West? ")
+answer = ("Sounds good!")
+action = ("What movement would you like yo do? The only option in this room is walk. ")
+walk = ("Walking!")
+ballroomMessage = ("You are currently in the Grand Ball Room. ")
+closetMessage = ("You are currently in the Closet. ")
+endingMessage = ("Sorry, you cannot move in that direction. Please choose another direction. ")
+
+def movements():
+  global row, col
+  print(input(askDirection))
+  print(answer)
+  if askDirection == "South":
+    row -= 1
+  elif askDirection == "North":
+    row += 1
+  elif askDirection == "East":
+    col += 1
+  elif askDirection == "West":
+    col -= 1
+  else:
+    print(endingMessage)
+  
+# Main
+current_location = HauntedMansion[row][col]
+
+if current_location == "Entrance":
+  print(welcomeMessage)
+  print(input(action))
+  print(walk)
+  movements()
+elif current_location == "Grand Ball Room":
+  print(ballroomMessage)
+  print(input(action))
+  print(walk)
+  movements()
+elif current_location == "Closet":
+  print(closetMessage)
+  print(input(action))
+  print(walk)
+  movements()
