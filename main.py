a881e1de5c1c4e32891a56da94e0fb3039bf9de4
yifeizhao18 <122106660+yifeizhao18@users.noreply.github.com>
2023-03-28 15:29:46 +0000

    I edited the dictionaries

--- main.py
+++ main.py
@@ -37,10 +37,28 @@ actionChoice = {
   "heal" : "Healing With Medicine!"
 }
 
+# dictionary for characters
+possibleCharacter = {
+  "ghost1" : {
+    "description" : "This is the most common ghost in the mansion. You flashlight will be enough to shine them away from you. ",
+    "locationMap" : "[(1,3), (0,1), (3,1), (2,0), (1,2), (3,2), (3,3)]",
+  },
+  "ghost2" : {
+    "description" : "These are a little bit better than the basic ghosts. These have lived in the mansion for the longest time. You need to kill them with the knife. GOOD LUCK! ",
+    "locationMap" : "[(0,0), (2,1), (0,2), (0,3), (2,3), (0,4), (2,4)]",
+  },
+  "theGhost" : {
+    "description" : "OH NO!! These are the ghosts you DO NOT want to come across. They can kill you within seconds, and you will not even notice it. Unfortunately, you can only kill them with the sword. ",
+    "locationMap" : "[(1,0), (3,0), (1,1), (0,3), (1,4)]"
+  }
+}
+
 # dictionary for the objects
 objectChoice = {
   "Treasure Box" : {
     "description" : "The user will be able to find the key that will lead them out of the room. ",
+    "possibleItems" : "["knife", "sword", "key", "flashlight"]"
+    
   },
   "Medicine" : {
     "description" : "The user will be able to find medicine to heal themselves. ",
@@ -55,44 +73,37 @@ objectChoice = {
 # dictionary for the map-tile
 roomsHauntedMansion = {
   "Entrance" : {
-    "description" : "Welcome To The Haunted House! You are currently at the Entrance. ",
-    "location" : "(2,2)",
+    "description" : "Welcome To The Haunted House! You are currently at the Entrance. Your goal is to escape this mansion before you die. ",
     "num_treasure" : "0", 
     "num_med" : "3"
   }, 
   "Grand Ball Room" : {
-    "description" : "You are currently in the Grand Ball Room. ",
-    "location" : ["(0,0)", "(0,2)", "(2,4)"],
+    "description" : "You are currently in the Grand Ball Room. This is the largest room in this house, maybe you want to explore it. BE CAREFUL!! ",
     "num_treasure" : "1",
     "num_med" : "2,"
   },
   "Closet" : {
-    "description" : "You are currently in the Closet. ",
-    "location" : ["(0,1)", "(3,1)", "(1,3)"],
+    "description" : "You are currently in the Closet. This is the smallest room in this house. All of the clothes in the closet... What can I find here? ",
     "num_treasure" : "1",
     "num_med" : "3",
   },
   "Master Bedroom" : {
-    "description" : "You are currently in the Master Bedroom. ",
-    "location" : ["(1,0)", "(3,0)","(1,3)"],
+    "description" : "You are currently in the Master Bedroom. This is where the previous couples stayed before they were never seen ever again... ",
     "num_treasure" : "2",
     "num_med" : "2",
   },
   "Rooftop" : {
-    "description" : "You are currently at the Rooftop of this mansion. ",
-    "location" : ["(2,1)", "(2,3)", "(0,4)"],
+    "description" : "You are currently at the Rooftop of this mansion. This is the highest place of this mansion. Be careful NOT to fall down... ",
     "num_treasure" : "1",
     "num_med" : "2",
   },
   "Bathroom" : {
-    "description" : "You are currently in the Bathroom. The room of slaughter. ",
-    "location" : ["(2,0)", "(1,2)", "(3,2)", "(3,3)"],
+    "description" : "You are currently in the Bathroom. The room of slaughter. Be careful NOT to be the next one... ",
     "num_treasure" : "3",
     "num_med" : "2",
   },
   "Exit" : {
     "description" : "Thank you for playing. Bye! ",
-    "location" : "(3,4)",
     "num_treasure" : "1",
     "num_med" : "1",
   }
